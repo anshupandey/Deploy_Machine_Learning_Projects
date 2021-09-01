410dbdf6d13377f087d96b74d3390729aab62c4c
Anshu Pandey <anshuspandey@gmail.com>
2021-09-01 10:15:10 +0530

    modified with improved HTML visual

--- mnist_flask_project/clientcode.py
+++ mnist_flask_project/clientcode.py
@@ -7,6 +7,6 @@ Created on Wed May  1 11:19:30 2019
 
 import requests
 url = "http://127.0.0.1:5000"
-data = {"image":open("zero.jpg","rb")}
+data = {"image":open("sample_image.png","rb")}
 response = requests.post(url,files=data)
 print(response.content)
